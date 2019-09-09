56f47d2104a9a983207a283b521f07bcb410234c
DerKO <Dr.KnockOut99@gmail.com>
2019-09-09 10:39:33 -0600

    Initial Commit (starting with copy of WorldRecord component)

---
+++ LiveSplit.Leaderboard/UI/Components/LeaderboardFactory.cs
@@ -0,0 +1,27 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+
+[assembly:ComponentFactory(typeof(LiveSplit.Leaderboard.UI.Components.LeaderboardFactory))]
+
+namespace LiveSplit.Leaderboard.UI.Components
+{
+    public class LeaderboardFactory : IComponentFactory
+    {
+        public string ComponentName => "Leaderboard";
+
+        public string Description => "Shows the Leaderboard for the run";
+
+        public ComponentCategory Category => ComponentCategory.Information;
+
+        public IComponent Create(LiveSplitState state) => new LeaderboardComponent(state);
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.WorldRecord.xml";
+
+        public string UpdateURL => "http://livesplit.org/update/";
+
+        public Version Version => Version.Parse("1.7.5");
+    }
+}
